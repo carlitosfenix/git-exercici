38d69ffa4405a7679cc46c3b4c8c862bdde774f1
Carlos Acevedo <carlos.acevedo.jimenez@gmail.com>
2020-02-14 13:15:42 +0100

    basicos js y C# segundo dia. Repasar colecciones

--- C_SharpMasJS/CSharp_1_Variables/1-ConsoleAppVariables/1-ConsoleAppVariables/Program.cs
+++ C_SharpMasJS/CSharp_1_Variables/1-ConsoleAppVariables/1-ConsoleAppVariables/Program.cs
@@ -9,9 +9,103 @@ namespace _1_ConsoleAppVariables
             var numA = 5;
             var numB = 6;
             var suma = numA + numB;
-            Console.WriteLine("Hello World! "+suma);
+            Console.WriteLine("Hello World! " + suma);
 
-            Console.ReadKey();
+            // Console.ReadKey();
+
+            variablesPorAsignacion();
+            variablesTipadas();
+            parseEntradaNumero();
+            operadores();
+            sayHello();
+            var resultado = Suma(45,20);
+            Console.WriteLine($"El resultado del método suma es: {resultado}");
+
+
+
+
         }
+
+
+        public static void operadores() {
+            /*Veamos que podemos hacer lo mismo que en java*/
+
+            int intOne = 1;
+            var intTwo = 2;
+
+            intOne += 3;
+            intTwo++;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("La variable intOne vale: " + intOne++);
+            Console.WriteLine("La variable intOne vale: " + intOne);
+            Console.WriteLine("-------------------------------------");
+
+
+        }
+
+        public static void sayHello() {
+            var textOut = "Hello";
+            Console.WriteLine("Saludo" + textOut);
+            Console.WriteLine($"Saludo-2: {textOut}");
+            Console.WriteLine(String.Format("Saludo-3 {0}", textOut));
+
+        }
+
+        public static int Suma(int a, int b){
+
+            return a+b;
+        }
+
+        
+    public static void variablesPorAsignacion(){
+        var entero = 23;
+        var decimales = 45.5m;//sufijo m para tipo decimal
+        var doble=0.5d;
+        var fechaTiempo = DateTime.Now;
+        var texto = "ejemplo de texo"; 
+        var caracter = 'u'; //comilla simple
+        var siOno = true;
+        Console.WriteLine("Variables por asignacion: "+entero+", "+decimales+", "+fechaTiempo+", "+doble+", "+texto+", "+caracter+", "+siOno );  
     }
+
+    public static void variablesTipadas(){
+        int entero =23;
+        decimal decimales = 45.5m;
+        double doble=0.5d;
+        DateTime fechaTiempo = DateTime.Now;
+        DateTime fechaTiempo2= new DateTime();
+        String texto = "ejemplo de texo"; 
+        char caracter = 'u'; //comilla simple
+        bool siOno = true;
+        Console.WriteLine("Variables tipadas: "+entero+", "+decimales+", "+fechaTiempo2+", "+doble+", "+texto+", "+caracter+", "+siOno );
+    }
+
+        public static void parseEntradaNumero() {
+
+            Console.WriteLine("Entra un número");
+            var entrada = Console.ReadLine();
+            int enteroCorrecto;
+            if (int.TryParse(entrada, out enteroCorrecto))
+            {
+                Console.WriteLine("conversión correcta: " + enteroCorrecto);
+            }
+            else
+            {
+                Console.WriteLine("No has entrado un número filtrada por TryParse");
+            }
+
+            try
+            {
+                var entero = int.Parse(entrada);
+
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("No has entrado un número, petada en try Catch, error = " + e);
+            }
+
+        } 
+    }
+
 }
--- C_SharpMasJS/Js_1_Variables/js/1-variables.js
+++ C_SharpMasJS/Js_1_Variables/js/1-variables.js
@@ -8,6 +8,42 @@ var sel = document.getElementById('js1');
 sel.innerHTML= "texto posterior ejecución js, la suma es: "+suma;
 sel.style.color ="blue";
 
+primeraVariables ();
+
+operadores();
+
+
+}
+
+function operadores (){
+    var intOne = 1;
+    var intTwo = 2;
+    
+    intOne += 3;
+    intTwo++;
+    console.log("La variable intOne vale: " + intOne++);
+    console.log("La variable intOne vale: " + intOne);
+
+}
+
+function primeraVariables(){
+    var entero = 23;
+    var decimales = 45.5;//sufijo m para tipo decimal
+    var doble=0.5;
+    var fechaTiempo = new Date();
+    var texto = "ejemplo de texo"; 
+    var caracter = 'u'; //comilla simple
+    var siOno = true;
+    console.log("Variables tipadas: "+entero+", "+decimales+", "+fechaTiempo+", "+doble+", "+texto+", "+caracter+", "+siOno );
+    var sel = document.getElementById('js2');
+    sel.innerHTML= "Variables por asignacion siempre en js: "+entero+", "+decimales+", "+fechaTiempo+", "+doble+", "+texto+", "+caracter+", "+siOno ;
+    sel.style.color ="orange";
+    var texto = "Si no es un numero JS devolvera NaN: ";
+    var entero = parseInt(texto,0);
+    var sel = document.getElementById('parse');
+    sel.innerHTML= "Resultado del parseInt: "+entero;
+    sel.style.color ="green";
+    
 
 
 }
