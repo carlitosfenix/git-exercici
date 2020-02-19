3de869097cd06d5c96887c9f4024d27e5d4efe97
Carlos Acevedo <carlos.acevedo.jimenez@gmail.com>
2020-02-19 14:06:06 +0100

    Calculadoras en C# y JS básicas. Next: mirar ejemplos de Jose para mejorar presentación y código

--- C_SharpMasJS/CSharp_2_Calculadora/CalculadoraWpfApp1/CalculadoraWpfApp1/MainWindow.xaml.cs
+++ C_SharpMasJS/CSharp_2_Calculadora/CalculadoraWpfApp1/CalculadoraWpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,78 @@ namespace CalculadoraWpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        double siNoNumA = 0.0; double siNoNumB = 0.0;
+        
+       
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOClick(object sender, RoutedEventArgs e)
+        {
+            var operacion = (sender as Button).Content;
+
+            ObtenerNumeros();
+
+            switch (operacion)
+            {
+                case "+":
+                    Suma();
+                    break;
+                case "-":
+                    Resta();
+                    break;
+                case "*":
+                    Producto();
+                    break;
+                case "/":
+                    Division();
+                    break;
+            }
+           
+              
+        }
+
+        private bool ObtenerNumeros()
+        {
+            var numeroA = Double.TryParse(numA.Text, out siNoNumA);
+            var numeroB = Double.TryParse(numB.Text, out siNoNumB);
+            return (numeroA && numeroB);
+        }
+
+        private void Suma() 
+        {
+            var suma = siNoNumA+ siNoNumB;
+            MessageBox.Show($"El resultado es: {suma}");
+            Console.WriteLine($"El resultado es: {suma}");
+            numResultado.Text =""+suma;
+        }
+
+        private void Resta()
+        {
+            var resta = siNoNumA - siNoNumB;
+            MessageBox.Show($"El resultado es: {resta}");
+            Console.WriteLine($"El resultado es: {resta}");
+            numResultado.Text = "" + resta;
+        }
+
+        private void Producto()
+        {
+            var producto = siNoNumA * siNoNumB;
+            MessageBox.Show($"El resultado es: {producto}");
+            Console.WriteLine($"El resultado es: {producto}");
+            numResultado.Text = "" + producto;
+        }
+
+        private void Division()
+        {
+            var division = siNoNumA / siNoNumB;
+            MessageBox.Show($"El resultado es: {division}");
+            Console.WriteLine($"El resultado es: {division}");
+            numResultado.Text = "" + division;
         }
     }
 }
--- C_SharpMasJS/Js_2_Calculadora/js/calculadora.js
+++ C_SharpMasJS/Js_2_Calculadora/js/calculadora.js
@@ -1,4 +1,57 @@
+var numeroAHtml = Object;
+var numeroBHtml;
+var htmlSalida;
+var textoSalida;
+
 function inicio()
 {
+
+    numeroAHtml = document.getElementById("txtNum1");
+    numeroBHtml = document.getElementById("txtNum2");
+    htmlSalida = document.getElementById("salida");
+    textoSalida = "";
     
- }
+}
+ 
+function suma()
+{
+    let numeroA = numeroAHtml.value;
+    let numeroB = numeroBHtml.value;
+    //Dos formas de convertir tipo
+    let numberSum = parseInt(numeroA) + (numeroB * 1);
+    htmlSalida.innerHTML = textoSalida +"La suma da: " + numberSum ;
+
+}
+
+function resta()
+{
+    let numeroA = numeroAHtml.value;
+    let numeroB = numeroBHtml.value;
+
+    //Dos formas de convertir tipo
+    let numberSum = parseInt(numeroA) - (numeroB * 1);
+    htmlSalida.innerHTML = textoSalida +"La suma da: " + numberSum ;
+
+}
+
+function producto()
+{
+    let numeroA = numeroAHtml.value;
+    let numeroB = numeroBHtml.value;
+
+    //Dos formas de convertir tipo
+    let numberSum = parseInt(numeroA) * (numeroB * 1);
+    htmlSalida.innerHTML = textoSalida +"La suma da: " + numberSum ;
+
+}
+
+function division()
+{
+    let numeroA = numeroAHtml.value;
+    let numeroB = numeroBHtml.value;
+
+    //Dos formas de convertir tipo
+    let numberSum = parseInt(numeroA) / (numeroB * 1);
+    htmlSalida.innerHTML = textoSalida +"La suma da: " + numberSum ;
+
+}
