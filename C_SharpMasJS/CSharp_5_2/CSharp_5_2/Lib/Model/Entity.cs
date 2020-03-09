1761712641aeb7f2b504356d676036989630c623
Carlos Acevedo <carlos.acevedo.jimenez@gmail.com>
2020-03-09 14:21:59 +0100

    Preparación 5_2 gestión de alumnos formulario pwf, clases del modelo y pendiente crear métodos CRUDS

---
+++ C_SharpMasJS/CSharp_5_2/CSharp_5_2/Lib/Model/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_5_2.Lib.Model
+{
+    public class Entity
+    {
+        private Guid _id;
+
+        public Guid Id { get => _id; set => _id = value; }
+    }
+}
---
+++ C_SharpMasJS/CSharp_5_2/CSharp_5_2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using CSharp_5_2.Lib.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSharp_5_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        double NotaAlumno;
+        string KeyWordOut = "runyoufools";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnRegistrarNota_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrarAlumno();
+        }
+
+        /**
+         * Versión previa al modelo
+         */
+        private void RegistrarAlumno()
+        {
+            RegistroAlumnos MiRegistroAlumnos = new RegistroAlumnos();
+            var notaAlumno = txtBoxNota.Text;
+            if (string.IsNullOrEmpty(notaAlumno) || double.TryParse(notaAlumno, out NotaAlumno))
+            {
+                if (!notaAlumno.Equals(KeyWordOut))
+                {
+                    // TODO: por ahora no se usa , crear dictionay para
+                    // guardar (string double) txtBoxNombre              
+                    MiRegistroAlumnos.RegistrarAlumno(NotaAlumno);
+                }
+                else
+                {
+                    btnRegistrarNewExam.IsEnabled = false;
+                    MiRegistroAlumnos.CalcularMaxMedMin();
+
+                }
+            }    
+        }
+
+      
+    }
+}
