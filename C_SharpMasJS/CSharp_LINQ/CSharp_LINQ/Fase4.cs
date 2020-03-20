5b1477de708e2c6542626097b45716455add4b5e
carlitosfenix <carlos.acevedo.jimenez@gmail.com>
2020-03-20 14:39:59 +0100

    LINQ en C#

---
+++ C_SharpMasJS/CSharp_LINQ/CSharp_LINQ/Fase4.cs
@@ -0,0 +1,81 @@
+﻿
+
+using System;
+using System.Linq;
+
+
+public class Fase4
+{
+    private string[] _nombres;
+    public static string Separador = "*****************************************************";
+
+    public string[] Nombres { get => _nombres; set => _nombres = value; }
+
+    public Fase4(string[] miArray)
+    {
+        Nombres = miArray;
+    }
+    ///<summary>
+    ///Entra una letra y filtraré por las que comiencen por dicha letra
+    ///</summary>
+    ///<param name="letra"> letra inicio,entrala en minúscula si quieres que se considere mayúscula y minúscula.</param>
+    ///<returns> void </returns>
+    public void ComeinzanPor(string letra)
+    {
+        //Con hibernate
+        /*
+        var comienzanO = from nomO in Nombres
+                         where SqlMethods.Like(nomO, $"{letra}%") || SqlMethods.Like(nomO, $"{letra.ToUpper()}%")
+                         orderby nomO
+                         select nomO;
+        Console.WriteLine("ComeinzanPorO() con Hibernate " + Fase4.Separador);
+        foreach (var result in comienzanO)
+        {
+            Console.WriteLine(result);
+        }*/
+
+       
+        //Con fluido o fluent
+        var coienzanOFluent = Nombres
+            .Where(nomO => nomO.StartsWith("o") || nomO.StartsWith("O"))
+            .Select(nomO => nomO);
+        Console.WriteLine("ComeinzanPorO() con fluent " +Separador);
+        foreach (var result in coienzanOFluent)
+        {
+            Console.WriteLine(result);
+        }
+    
+    }
+    /// <summary>
+    /// Filtraré por la longitud entrada
+    /// </summary>
+    /// <param name="longitud"></param>
+    public void MasDeXLetras(int longitud)
+    {
+        var tmpIEnumerable = from nombre in Nombres
+                             where nombre.Length>longitud
+                             select nombre;
+        Console.WriteLine($"MasDe {longitud} Letras() " + Fase4.Separador);
+        foreach (var result in tmpIEnumerable)
+        {
+            Console.WriteLine(result);
+        }
+       
+
+    }
+
+    public void OrderDesc() 
+    {
+        var tmpDesc = from nomDesc in Nombres
+                      orderby nomDesc descending
+                      select nomDesc;
+        Console.WriteLine("OrderDesc()" + Fase4.Separador);
+
+        foreach (var result in tmpDesc) 
+        {
+            Console.WriteLine(result);
+
+        }
+    }
+
+}
--- C_SharpMasJS/CSharp_LINQ/CSharp_LINQ/Program.cs
+++ C_SharpMasJS/CSharp_LINQ/CSharp_LINQ/Program.cs
@@ -9,58 +9,116 @@ namespace CSharp_LINQ
 
         static int[] Numeros = { 2, 6, 8, 4, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
         static int[] Pares;
-        static string Separador = "*****************************************************";
+        static int[] Menores5;
+        static int[] Mayores5;
+        
 
         static void Main(string[] args)
         {
             Console.WriteLine("CSharp LINQ y estilo fluido o Fluent");
-            guardarNumerosPares();
-            mostrarConsolaNumeroPares();
-            calcularMediaMaxMin();
+            GuardarNumerosPares();
+            MostrarConsolaNumeroPares();
+            CalcularMediaMaxMin();
+            NumerosMayores5();
+            NumerosMenores5(); //Al pobre 5 no lo tenemos en cuenta, lo discriminamos
+            //Fase 4. Trabajando con Strings desde LINQ
+            TrabajandoConStrings();
         }
 
-      
-
-        private static void mostrarConsolaNumeroPares()
+        private static void TrabajandoConStrings()
         {
-            Console.WriteLine("mostrarConsolaNumeroPares() " + Separador);
-            foreach (int elemento in Pares) {
+            string[] nombres = { "David", "Sergio", "Laura", "Maria", "Oscar", "Julia", "Oriol" };
+            Fase4 miStrings = new Fase4(nombres);
+            miStrings.ComeinzanPor("o");
+            miStrings.MasDeXLetras(5);
+            miStrings.OrderDesc();
+        }
+
+        private static void MostrarConsolaNumeroPares()
+        {
+            Console.WriteLine("mostrarConsolaNumeroPares() " + Fase4.Separador);
+            foreach (int elemento in Pares)
+            {
                 Console.WriteLine($"{elemento}");
             }
         }
 
-        private static void guardarNumerosPares()
+        private static void GuardarNumerosPares()
         {
             var tmpPares = from par in Numeros
-                    where (par % 2) == 0
-                    orderby par
-                    select par;
+                           where (par % 2) == 0
+                           orderby par
+                           select par;
 
-            Console.WriteLine("guardarNumerosPares() " + Separador);
+            Console.WriteLine("guardarNumerosPares() " + Fase4.Separador);
             int longitud = tmpPares.Count();
             Pares = new int[longitud];
             var count = 0;
-            foreach (var result in tmpPares) {
+            foreach (var result in tmpPares)
+            {
                 Console.WriteLine(result);
-                Pares[count]= result;
+                Pares[count] = result;
                 count++;
             }
 
 
         }
 
-        private static void calcularMediaMaxMin()
+        private static void CalcularMediaMaxMin()
         {
             var tmpIEnumerable = from num in Pares
-                        select num;
+                                 select num;
             int max = tmpIEnumerable.Max();
             int min = tmpIEnumerable.Min();
             var avg = tmpIEnumerable.Average();
 
-            Console.WriteLine("calcularMediaMaxMin() " + Separador);
-            Console.WriteLine($"El valor Max: {max}, el Min: {min} y el Medio: {avg}" );
+            Console.WriteLine("calcularMediaMaxMin() " + Fase4.Separador);
+            Console.WriteLine($"El valor Max: {max}, el Min: {min} y el Medio: {avg}");
 
 
         }
+        #region fase1
+        private static void NumerosMayores5()
+        {
+            Console.WriteLine("NumerosMayores5() " + Fase4.Separador);
+            var mayores5 = from num in Numeros
+                           where num > 5
+                           orderby num
+                           select  num ; // new {num} cuidado, si hacemos un new crearemos un tipo anonimo dificil de convertir
+
+            int longitud = mayores5.Count();
+            Mayores5 = new int[longitud];
+            int posicion = 0;
+            foreach (var elemento in mayores5)
+            {
+                Console.WriteLine($"{elemento}");
+                Mayores5[posicion] = elemento;
+                posicion++;
+
+
+            }
+
+        }
+        private static void NumerosMenores5()
+        { 
+            //En este caso lo hacemos fluido
+            Console.WriteLine("NumerosMenores5() " + Fase4.Separador);
+            var menores5 = Numeros
+                .Where(num => num < 5)
+                .OrderBy(num => num)
+                .Select(num => num); // new { num });creamos tipo anónimo
+
+            int longitud = menores5.Count();
+            Menores5 = new int[longitud];
+            int posicion = 0;
+            foreach (var elemento in menores5)
+            {
+                Console.WriteLine($"{elemento}");
+                Menores5[posicion] = elemento;
+                posicion++;
+
+            }
+        }
+        #endregion fase 1
     }
 }
